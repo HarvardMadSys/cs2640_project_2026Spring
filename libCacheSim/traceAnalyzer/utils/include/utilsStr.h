6aede7fd4b7e75f36b38be2fb1316ef224923fab
Juncheng Yang <1a1a11a@users.noreply.github.com>
2025-06-21 11:54:09 -0400

    1a1a11a/cleanup CMakeList (#203)
    * cleanup cmakelists
    
    * add dev env install
    
    * cleanup continue
    
    * cleanup library installation
    
    * remove admission and prefetch link library
    
    * update eviction camke
    remove private
    
    * fix clang-tidy
    
    * separate dev dependency; reformat
    
    * many fixes
    
    * fix bugs and disable mrcProfiler test
    
    * fix formatting
    
    * fix thread init issue
    
    * add export-dynamic to cachesim
    
    * fix a double-free bug
    
    * remove cmake files that are not needed
    
    * remove cmake redundant lib
    
    * fix an issue in rebase

--- libCacheSim/traceAnalyzer/utils/include/utilsStr.h
+++ libCacheSim/traceAnalyzer/utils/include/utilsStr.h
@@ -4,11 +4,12 @@
 // Created by Juncheng Yang on 6/19/20.
 //
 
-#include "const.h"
-#include <cstring>
-#include <cstdio>
-#include <cstdlib>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "libCacheSim/const.h"
 
 namespace utilsStr {
 
@@ -19,26 +20,25 @@ namespace utilsStr {
  * @param str a 8 byte char array
  */
 static inline void convert_size_to_str(long long size, char *str) {
-
-    if (size >= TiB) {
-        sprintf(str, "%.0lf TiB", (double) size / TiB);
-    } else if (size >= GiB) {
-        sprintf(str, "%.0lf GiB", (double) size / GiB);
-    } else if (size >= MiB) {
-        sprintf(str, "%.0lf MiB", (double) size / MiB);
-    } else if (size >= KiB) {
-        sprintf(str, "%.0lf KiB", (double) size / KiB);
-    } else {
-        sprintf(str, "%lld B", size);
-    }
+  if (size >= TiB) {
+    sprintf(str, "%.0lf TiB", (double)size / TiB);
+  } else if (size >= GiB) {
+    sprintf(str, "%.0lf GiB", (double)size / GiB);
+  } else if (size >= MiB) {
+    sprintf(str, "%.0lf MiB", (double)size / MiB);
+  } else if (size >= KiB) {
+    sprintf(str, "%.0lf KiB", (double)size / KiB);
+  } else {
+    sprintf(str, "%lld B", size);
+  }
 }
 
 static inline uint64_t str_to_u64(const char *start, size_t len) {
-    uint64_t n = 0;
-    while (len--) {
-        n = n * 10 + *start++ - '0';
-    }
-    return n;
+  uint64_t n = 0;
+  while (len--) {
+    n = n * 10 + *start++ - '0';
+  }
+  return n;
 }
 
-}// namespace utilsStr
+}  // namespace utilsStr
